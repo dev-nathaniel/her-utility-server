15a25e2c2a8088e31b6723fde7b39229f1d43582
Olowodev <nathanielolowofoyeku@gmail.com>
2025-11-14 19:32:23 +0000

    filler get all model function

--- src/services/invites.ts
+++ src/services/invites.ts
@@ -1,1 +1,12 @@
+import type { Request, Response } from "express";
 import Invite from "../models/Invite.js";
+
+export async function getInvites(request: Request, response: Response) {
+  console.log("Get users endpoint hit");
+  try {
+    const users = await Invite.find({}, { password: 0 }).sort({createdAt: -1});
+    response.status(200).send({message: 'Successful', users});
+  } catch (error) {
+    response.status(400).send({ message: "Error fetching users" });
+  }
+}
--- src/services/utilities.ts
+++ src/services/utilities.ts
@@ -1,1 +1,12 @@
+import type { Request, Response } from "express";
 import Utility from "../models/Utility.js";
+
+export async function getUtilities(request: Request, response: Response) {
+  console.log("Get users endpoint hit");
+  try {
+    const users = await Utility.find({}, { password: 0 }).sort({createdAt: -1});
+    response.status(200).send({message: 'Successful', users});
+  } catch (error) {
+    response.status(400).send({ message: "Error fetching users" });
+  }
+}
